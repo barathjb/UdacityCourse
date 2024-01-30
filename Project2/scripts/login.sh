f2209292bdf1be0107d5ce9ce39da46bbf17a7c4
barathjb <cbjbarath@gmail.com>
2024-01-30 07:50:43 +0000

    Project2

---
+++ Project2/scripts/login.sh
@@ -0,0 +1,4 @@
+aws configure set aws_access_key_id AKIATUW2UOM2JVZLZGO7
+aws configure set aws_secret_access_key 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
+aws configure set region us-east-1
+aws configure list
